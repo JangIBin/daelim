2ad4706bf0b258eaac0ad8623920fbbab0d1ed0e
GamJarin <59593043+JangIBin@users.noreply.github.com>
2020-08-04 15:42:25 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { getProblemList } from './api/problems';
+import CodeMirror from 'react-codemirror';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
+
+// prop
+function ProblemDetail({ problem }) {
+  return (
+    <div className="problem">
+      <section className="description">
+        <h3>{problem.title}</h3>
+        <p>{problem.description}</p>
+      </section>
+      <section className="code-editor">
+        <CodeMirror
+          value={'function solution () {}'}
+          options={{
+            mode: 'javascript'
+          }}
+        />
+        <button>제출</button>
+      </section>
+    </div>
+  );
+}
 
 function App() {
+  const [ problems, setProblems ] = useState([]);
+  const [ selectedProblemId, setSelectedProblemId ] = useState(null);
+
+  const [ selectedProblem ] = problems.filter(data => data.id === selectedProblemId);
+
+  useEffect(function () {
+    async function getProblems () {
+      const data = await getProblemList();
+      setProblems(data);
+    }
+
+    getProblems();
+  }, []); // dependency...
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        selectedProblemId === null &&
+        <>
+          <nav>
+            Codewars
+          </nav>
+          <ul>
+            {
+              problems.map(function (problem) {
+                return (
+                  <li key={problem.id}>
+                    <h3>{problem.title}</h3>
+                    <button onClick={() => setSelectedProblemId(problem.id)}>문제풀기</button>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </>
+      }
+      {
+        selectedProblemId !== null &&
+        <ProblemDetail problem={selectedProblem} />
+      }
     </div>
   );
 }
